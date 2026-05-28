35cdc63aec45488a9cd93e4b9917d2e56bac6515
Apriyudha <apriyudha307@gmail.com>
2026-05-28 12:39:16 +0700

    Login page

---
+++ auth/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="../includes/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <img src="../assets/starwars-logo.png" alt="Logo" class="sw-logo">
+    <div class="container-auth">
+        <h1 class="title-auth">LOGIN</h1>
+        <p class="subtitle-auth">Enter the galaxy and explore the universe</p>
+        <form action="login.php" method="POST"><br>
+            <input type="text" id="username" name="username" placeholder="username" class="input-auth" required><br>
+            <input type="password" id="password" name="password" placeholder="password" class="input-auth" required><br>
+            <label for="remember" class="remember-label"><input type="checkbox" id="remember" name="remember">Stay signed in</label><br>
+            <button type="submit" id="signin" class="red-btn">SIGN-IN</button>
+        </form>
+        <button onclick="window.location.href='register.php'" type="button" class="transparent-btn">CREATE ACCOUNT</button>
+    </div>
+</body>
+</html>
