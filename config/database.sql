e676173cc674d29267b44c1b09cba7592540d84f
nayla-kirani <nayla.kirani@mhs.unsoed.ac.id>
2026-06-11 04:39:56 +0700

    Menambahkan file database.sql

--- config/database.sql
+++ config/database.sql
@@ -1,29 +1,165 @@
-CREATE TABLE users (
-    id_user INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('admin','user') DEFAULT 'user'
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 10, 2026 at 09:34 PM
+-- Server version: 8.0.43
+-- PHP Version: 8.2.28
 
-CREATE TABLE characters (
-    id_character INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    image VARCHAR(255)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE movies (
-    id_movie INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    year YEAR,
-    rating FLOAT,
-    image VARCHAR(255)
-);
 
-CREATE TABLE planets (
-    id_planet INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT,
-    image VARCHAR(255)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `starwars_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `characters`
+--
+
+CREATE TABLE `characters` (
+  `id_character` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `characters`
+--
+
+INSERT INTO `characters` (`id_character`, `name`, `description`, `image`) VALUES
+(1, 'Darth Vader', 'Sith Lord paling kuat', 'character1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `id_movie` int NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `year` year DEFAULT NULL,
+  `rating` float DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `movies`
+--
+
+INSERT INTO `movies` (`id_movie`, `title`, `year`, `rating`, `image`) VALUES
+(1, 'The Empire Strikes Back', 1980, 8.7, 'movie1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `planets`
+--
+
+CREATE TABLE `planets` (
+  `id_planet` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `planets`
+--
+
+INSERT INTO `planets` (`id_planet`, `name`, `description`, `image`) VALUES
+(1, 'Tatooine', 'Planet gurun tempat Luke Skywalker berasal.', 'planet1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','user') DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `username`, `email`, `password`, `role`) VALUES
+(1, 'nayla', 'nayla@gmail.com', '$2y$10$kpXmPT5z5HGxm4gs3cKvKOIS5gBcED81M5qzyab9gFFNUuf9hX/Ta', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `characters`
+--
+ALTER TABLE `characters`
+  ADD PRIMARY KEY (`id_character`);
+
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id_movie`);
+
+--
+-- Indexes for table `planets`
+--
+ALTER TABLE `planets`
+  ADD PRIMARY KEY (`id_planet`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `characters`
+--
+ALTER TABLE `characters`
+  MODIFY `id_character` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id_movie` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `planets`
+--
+ALTER TABLE `planets`
+  MODIFY `id_planet` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
